fb99f57fab319e1b86fe9d315205238c96181929
thtrucpn <pnttruc258@gmail.com>
2024-09-16 14:02:42 +0700

    demo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Demo Website!</h1>
+        <p>This is a simple React demo website.</p>
+        <button onClick={() => alert('Hello World!')}>Click me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
